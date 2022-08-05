ce925757e9f98ed5cc41db090428872b2505ca8e
ElenaHM21 <95474804+ElenaHM21@users.noreply.github.com>
2022-08-05 09:44:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/bodaalbertoymaria'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/bodalbertoymaria'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/bodaalbertoymaria/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/bodaalbertoymaria/'}),
+);
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
